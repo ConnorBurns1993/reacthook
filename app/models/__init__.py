62cf7c18a0a0cf5bcbfe95578df9f424389f70df
Connor Burns <cburns1993@gmail.com>
2022-08-02 15:09:56 -0700

    sending the tables to heroku

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
 from .db import db
 from .user import User
 from .post import Post
+from .postlike import PostLike
 from .comment import Comment
+from .commentlike import CommentLike
 from .friend import Friend
--- app/models/friend.py
+++ app/models/friend.py
@@ -8,10 +8,8 @@ class Friend(db.Model):
     user_b_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     #relationships
-    user = db.relationship('User', back_populates='posts')
     user_a = db.relationship('User', back_populates='first_friend', foreign_keys=[user_a_id])
     user_b = db.relationship('User', back_populates='second_friend', foreign_keys=[user_b_id])
-    comments = db.relationship('Comment', back_populates='post')
 
     def to_dict(self):
         return {
